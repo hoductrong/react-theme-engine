31ae124bbb5cf152527735fa7bdd56a84f855968
Trong Ho <trong.hoduc@gmail.com>
2023-10-07 22:36:15 +0700

    fix: check ssr

--- src/utils/setCSSVariables.ts
+++ src/utils/setCSSVariables.ts
@@ -2,6 +2,8 @@ import { Theme } from "src/types";
 import { flatObject } from "./flatObject";
 
 export function setCSSVariables<T extends Theme[keyof Theme]>(theme: T) {
+  if (typeof window === 'undefined') return;
+  
   const flattenTheme = flatObject(theme);
 
   Object.entries(flattenTheme).forEach(([key, value]) => {
